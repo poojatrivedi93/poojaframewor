2aa1a29b24cd98cbd2d2679654f43c5e44aa9daf
Anbuit002 <Anbuit002@www.easeus.com>
2025-12-29 13:33:45 +0530

    Ranjithdepartment

---
+++ src/test/java/org/RanjithNewClass.java
@@ -0,0 +1,11 @@
+package org;
+
+public class RanjithNewClass {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		System.out.println(" i have done some changes");
+		
+	}
+
+}
